3ac127f7c05af000767f33a31830e6b14e1a585f
Digitechcyber.github.io <chamanjha2015@gmail.com>
2025-12-25 17:57:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
 from rembg import remove
+from PIL import Image
 import io
 
 app = FastAPI()
 
-# Frontend से जुड़ने के लिए अनुमति (CORS)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -20,6 +20,19 @@ def home():
 
 @app.post("/remove-bg")
 async def remove_bg(file: UploadFile = File(...)):
-    input_image = await file.read()
-    output_image = remove(input_image)
+    input_data = await file.read()
+    img = Image.open(io.BytesIO(input_data))
+
+    # इमेज को छोटा करना ताकि सर्वर क्रैश न हो
+    max_size = 800
+    if img.width > max_size:
+        ratio = max_size / float(img.width)
+        new_height = int(float(img.height) * float(ratio))
+        img = img.resize((max_size, new_height), Image.LANCZOS)
+
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='PNG')
+    output_image = remove(img_byte_arr.getvalue())
+
     return Response(content=output_image, media_type="image/png")
+
